04c8c3e808d4ba2c28cc5f63cf24fd6a0387482c
Ricardo Roldán Fdez <ricky395@users.noreply.github.com>
2016-10-07 17:36:51 +0200

    Add files via upload

---
+++ AttackScript.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackScript : MonoBehaviour {
+    
+
+    /// <summary>
+    /// Character transform
+    /// </summary>
+    Transform tr;
+
+    /// <summary>
+    /// Character animator
+    /// </summary>
+    Animator animator;
+
+    /// <summary>
+    /// Minimum distance for the character to attack an enemy
+    /// </summary>
+    public float attackDistance = 1;
+
+    /// <summary>
+    /// Fire rate of character
+    /// </summary>
+    public float attackingSpeed = 2;
+
+    /// <summary>
+    /// Defines if the character is an enemy or not
+    /// </summary>
+    bool isPlayable;
+
+    /// <summary>
+    /// The layer the characters have
+    /// </summary>
+    string layerName;
+
+    /// <summary>
+    /// Direction of the projectile
+    /// </summary>
+    int direction;
+
+    /// <summary>
+    /// Detects if is shooting or not
+    /// </summary>
+    bool attacking = false;
+
+    /// <summary>
+    /// Reference to another class which manages different attack types
+    /// </summary>
+    AttackTypeByClass attackReference;
+
+    /// <summary>
+    /// Reference to the movements of characters
+    /// </summary>
+    SpriteMovement sm;
+
+    /// <summary>
+    /// Controls if the unit is the special unit 1
+    /// </summary>
+    public bool isGEO;
+
+    /// <summary>
+    /// Controls if the unit is the special unit 2
+    /// </summary>
+    public bool isStrong;
+
+    /// <summary>
+    /// Initializes all references and determines whether the unit is ally or enemy
+    /// </summary>
+    void Start () {
+
+        attackReference = GetComponent<AttackTypeByClass>();
+        tr = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+
+        if (this.gameObject.layer == LayerMask.NameToLayer("Hippie")) //this is an ally and the direction of bullets and the target units are asigned.
+        {
+            direction = 1;
+            layerName = "Cop";
+            isPlayable = true;
+        }
+
+        else if (this.gameObject.layer == LayerMask.NameToLayer("Cop"))//this is an enemy and the direction of bullets and the target units are asigned.
+        {
+            direction = -1;
+            layerName = "Hippie";
+            isPlayable = false;
+            sm = GetComponent<SpriteMovement>();
+        }
+
+	}
+	
+
+    /// <summary>
+    /// Check if the character have an enemy on his front to begin the attack
+    /// </summary>
+	void Update () {
+
+        Vector2 origin = tr.position;
+        
+        float xPos;
+
+        if (isStrong)
+            xPos = tr.position.x - 2;
+
+        else xPos = tr.position.x;
+
+        Debug.DrawLine(origin, new Vector2(xPos + direction * attackDistance, tr.position.y), Color.green);
+        attacking = Physics2D.Linecast(origin, new Vector2(xPos + direction * attackDistance, tr.position.y), 1 << LayerMask.NameToLayer(layerName)); //Detects if there is, at least, one enemy
+
+        if (attacking)
+            Attacking();
+
+        else NotAttacking();
+	}
+
+    void Attacking() //attack function
+    {
+        if (!isPlayable)
+        {
+            if (!isGEO) //if is not an advanced unit
+                sm.enabled = false; //sprite movement stops while attacking
+        }
+
+        if (!attackReference.GetIsFreeze())
+            animator.Play("attack");
+
+        else animator.Play("idle");
+    }
+
+    void NotAttacking() //manages what happens when a unit stops attacking
+    {
+        if (!isPlayable)
+        {
+            sm.enabled = true;
+        }
+        animator.Play("idle");
+    }
+
+    public bool GetIsAttacking() //getter of 'is attacking' boolean
+    {
+        return attacking;
+    }
+
+    public float GetAttackingSpeed() //attacking speed getter
+    {
+        return attackingSpeed;
+    }
+}
---
+++ SpriteMovement.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteMovement : MonoBehaviour {
+
+    /// <summary>
+    /// variable that manages the 'grid' movement
+    /// </summary>
+    float gridJumpMovement;
+
+    /// <summary>
+    /// Transform of the character. This script is attached to enemies, allies and projectiles
+    /// </summary>
+    Transform tr;
+
+    /// <summary>
+    /// Rigidbody of the GameObject
+    /// </summary>
+    Rigidbody2D rb;
+
+    /// <summary>
+    /// 'Velocity' of the GameObject
+    /// </summary>
+    public float timeBetweenMoves = 0.8f;
+
+    /// <summary>
+    /// Boolean that shows if the current Gameobject is ally or enemy
+    /// </summary>
+    public bool isEnemy;
+
+    /// <summary>
+    /// Timer
+    /// </summary>
+    float count;
+
+    /// <summary>
+    /// Movement state of GameObject
+    /// </summary>
+    bool isMoving = true;
+
+    /// <summary>
+    /// Initializes components and the movement direction
+    /// </summary>
+    void Start () {
+
+        if (isEnemy) //If the gameobject is an enemy or an enemy projectile, it moves on the opposite direction (left)
+            gridJumpMovement = -0.2f;
+
+        else gridJumpMovement = 0.2f;
+        
+        rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+
+	}
+
+    /// <summary>
+    /// Manages the time between every move
+    /// </summary>
+    void Update()
+    {
+        count += Time.deltaTime;
+
+        if (count > timeBetweenMoves)
+        {
+            Movement();
+            count = 0;
+        }
+    }
+	
+    /// <summary>
+    /// Moves the sprite
+    /// </summary>
+     void Movement()
+    {
+        if (isMoving)
+            tr.position = new Vector2(tr.position.x + gridJumpMovement, tr.position.y);
+    }
+
+    /// <summary>
+    /// Manages the colliders that the gameobject can collide with
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Limits")) //If the gameobject is out of the screen, its destroyed
+        {
+            if (this.gameObject.layer == LayerMask.NameToLayer("Cop"))
+                GameObject.Find("SceneManager").GetComponent<PlayerHealth>().UpdateHealth(-1);
+            
+            Destroy(this.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Movement setter
+    /// </summary>
+    /// <param name="state"></param>
+    public void SetMovement(bool state)
+    {
+        isMoving = state;
+    }
+}
